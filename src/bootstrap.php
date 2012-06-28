1ec85b08080e27746006e900e097d28b4a714844
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2012-06-28 15:42:15 +0300

    forgot use!!!

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,5 +2,7 @@
 
 require_once __DIR__  . '/../vendor/autoload.php';
 
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+
 $autoloader = new UniversalClassLoader();
 $autoloader->register();
