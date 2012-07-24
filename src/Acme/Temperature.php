6446b90e670fde3651210389a427511eb90296ee
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2012-07-24 21:27:56 +0200

    cosmetic to use phpcs (Symfony2). $ phpcs -sv --ignore=vendor,tests .

--- src/Acme/Temperature.php
+++ src/Acme/Temperature.php
@@ -1,22 +1,39 @@
 <?php
+
 namespace Acme;
 
-class Temperature {
+/**
+ * Temperature 
+ * 
+ * @author isidromerayo <isidro.merayo@gmail.com>
+ */
+class Temperature
+{
 
     private $_service;
 
+    /**
+     * Constructor 
+     * @param type $service
+     */
     public function __construct($service)
-  {
+    {
         $this->_service = $service;
     }
 
+    /**
+     * Calculate temperature's average
+     * @return integer
+     */
     public function average()
-  {
+    {
         $total = 0;
-        for ($i=0;$i<3;$i++) {
+        for ($i = 0; $i < 3; $i++) {
             $total += $this->_service->readTemp();
         }
-        return $total/3;
+
+        return $total / 3;
     }
+
 }
 
