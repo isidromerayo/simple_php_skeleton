f87f096d97ff14b4ae593cbd8e872e9e769afa1a
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2012-09-03 21:29:14 +0200

    change Service to namespace Acme\Service

--- tests/Acme/TemperatureTest.php
+++ tests/Acme/TemperatureTest.php
@@ -13,7 +13,7 @@ class TemperatureTest extends \PHPUnit_Framework_TestCase
      */
     public function simpleMock()
     {
-        $service = \Mockery::mock('service');
+        $service = \Mockery::mock('Acme\Service');
         $service->shouldReceive('readTemp')->times(3)->andReturn(10, 12, 14);
         $temperature = new Temperature($service);
         $this->assertEquals(12, $temperature->average());
