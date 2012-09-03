297459a1a7d8a66eaf8a519826179d83463f5a67
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2012-09-03 22:08:46 +0200

    revert bad merge

--- tests/Acme/TemperatureTest.php
+++ tests/Acme/TemperatureTest.php
@@ -1,19 +1,24 @@
 <?php
 namespace Acme;
 
+/**
+ * Description of DummyTest
+ *
+ * @author isidromerayo
+ */
 class TemperatureTest extends \PHPUnit_Framework_TestCase
 {
-  /**
-   * @test
-   */
-  public function simpleMock()
-  {
-    $service = $this->getMock('\Acme\Service', array('readTemp'));
+    /**
+     * @test
+     */
+    public function simpleMock()
+    {
+    $service = $this->getMock('Acme\Service', array('readTemp'));
 
     $service->expects($this->any())
             ->method('readTemp')
             ->will($this->onConsecutiveCalls(10,12,14));
-    $temperature = new Temperature($service);
-    $this->assertEquals(12, $temperature->average());
-  }
+        $temperature = new Temperature($service);
+        $this->assertEquals(12, $temperature->average());
+    }
 }
