16bf30180fb19c419488cbb9cfee0092b291b3ac
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2012-09-06 20:23:18 +0200

    Revised code

--- tests/Acme/TemperatureTest.php
+++ tests/Acme/TemperatureTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Acme;
 
+use Acme\Temperature;
 /**
  * Description of DummyTest
  *
@@ -13,11 +14,11 @@ class TemperatureTest extends \PHPUnit_Framework_TestCase
      */
     public function simpleMock()
     {
-    $service = $this->getMock('Acme\Service', array('readTemp'));
-
-    $service->expects($this->any())
-            ->method('readTemp')
-            ->will($this->onConsecutiveCalls(10,12,14));
+        $service = \Phake::mock('Acme\Service');
+        \Phake::when($service)->readTemp()->thenReturn(10)
+                                            ->thenReturn(12)
+                                            ->thenReturn(14);
+        
         $temperature = new Temperature($service);
         $this->assertEquals(12, $temperature->average());
     }
--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -4,3 +4,6 @@ date_default_timezone_set('Europe/Madrid');
 error_reporting(E_ALL | E_STRICT);
 
 require_once __DIR__.'/../src/bootstrap.php';
+
+// registrar el cliente
+\Phake::setClient(Phake::CLIENT_PHPUNIT);
