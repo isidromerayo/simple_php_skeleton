0cbe71a121a7846b3eb9e5f5125a21b2b1c16d19
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2013-10-09 19:51:09 +0200

    Revised 'Mock' example

--- tests/unit/Acme/TemperatureTest.php
+++ tests/unit/Acme/TemperatureTest.php
@@ -8,10 +8,18 @@ namespace Acme;
  */
 class TemperatureTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @test
-     */
-    public function simpleMock()
+    /** @test */
+    public function simpleMockWithPHPUnit()
+    {
+        $service = $this->getMock('Acme\ServiceInterface', array('readTemp'));
+        $service->expects($this->any())
+            ->method('readTemp')
+            ->will($this->onConsecutiveCalls(10,12,14));
+        $temperature = new Temperature($service);
+        $this->assertEquals(12, $temperature->average());
+    }
+    /** @test */
+    public function simpleMockWithMockery()
     {
         $service = \Mockery::mock('Acme\Service');
         $service->shouldReceive('readTemp')->times(3)->andReturn(10, 12, 14);
