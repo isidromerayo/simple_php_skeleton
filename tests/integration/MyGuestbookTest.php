65425b1185c460192fb6e4309e62898639f21d4d
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2013-10-09 18:44:32 +0200

    restructure integration

--- tests/integration/MyGuestbookTest.php
+++ tests/integration/MyGuestbookTest.php
@@ -9,23 +9,27 @@ namespace Acme;
 class MyGuestbookTest extends \PHPUnit_Extensions_Database_TestCase
 {
 
+    private $pdo;
+    public function setUp() {
+        $this->pdo = new \PDO('sqlite::memory:');
+        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, 
+                              \PDO::ERRMODE_EXCEPTION);
+        $this->pdo->prepare("CREATE TABLE IF NOT EXISTS 'guestbook' (id int, content text, user text, created datetime)")->execute();
+        
+    }
     /**
      * @return PHPUnti_Extensions_Database_DB_IDatabaseConnection
      */
     public function getConnection()
     {
-        $pdo = new \PDO('sqlite::memory:');
-        $pdo->setAttribute(\PDO::ATTR_ERRMODE, 
-                              \PDO::ERRMODE_EXCEPTION);
-        $pdo->prepare("CREATE TABLE IF NOT EXISTS 'guestbook' (id int, content text, user text, created int)")->execute();
-        return $this->createDefaultDBConnection($pdo, ':memory:');
+        return $this->createDefaultDBConnection($this->pdo, ':memory:');
     }
     /**
      * @return PHPUnit_Extensions_Database_DataSet_IDataSet
      */
     public function getDataSet()
     {
-        return $this->createFlatXMLDataSet(dirname(__FILE__).'/_files/guestbook-seed.xml');
+        return $this->createFlatXMLDataSet(dirname(__FILE__).'/fixture/guestbook-seed.xml');
     }
     
     public function testNoGetDataSet() {
